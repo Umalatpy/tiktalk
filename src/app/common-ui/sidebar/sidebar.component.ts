58abd1205f84a80b2739a35371744f53eee0c3ac
Umalat <samsamow@gmail.com>
2025-01-04 17:29:26 +0300

    sidebar 2.50~

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {SvgIconComponent} from '../svg-icon/svg-icon.component';
-import {NgForOf} from '@angular/common';
+import {AsyncPipe, JsonPipe, NgForOf} from '@angular/common';
 import {SubscriberCardComponent} from './subscriber-card/subscriber-card.component';
 import {RouterLink} from '@angular/router';
+import {ProfileService} from '../../data/services/profile.service';
+import {Observable} from 'rxjs';
+import {Pageble} from '../../data/interfaces/pageble.interface';
+import {Profile} from '../../data/interfaces/profile.interface';
 
 @Component({
     selector: 'app-sidebar',
@@ -10,12 +14,18 @@ import {RouterLink} from '@angular/router';
     SvgIconComponent,
     NgForOf,
     SubscriberCardComponent,
-    RouterLink
+    RouterLink,
+    AsyncPipe,
   ],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService)
+
+  subscribers$ = this.profileService.getSubscribersShortList()
+
+  me = this.profileService.me
   menuItems = [
     {
       label: 'Моя страница',
