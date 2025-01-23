0471c41024b9fff329d7cd524b9ad14881974ced
Umalat <samsamow@gmail.com>
2025-01-23 21:46:31 +0300

    avatar-upload-finished

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {SvgIconComponent} from '../../../common-ui/svg-icon/svg-icon.component';
+import {DndDirective} from '../../../common-ui/directives/dnd.directive';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-avatar-upload',
   imports: [
-    SvgIconComponent
+    SvgIconComponent,
+    DndDirective,
+    FormsModule
   ],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss'
 })
 export class AvatarUploadComponent {
+  preview = signal<string>('/assets/imgs/avatar-placeholder.png')
+
+  avatar = null
+
+  fileBrowserHandler(event: Event) {
+    const file = (event.target as HTMLInputElement)?.files?.[0]
+    this.processFile(file)
+  }
+
+  onFileDroped(file: File) {
+    this.processFile(file)
+
+  }
+
+  processFile(file: File | null | undefined ) {
+
+    if (!file || !file.type.match('image')) return
+
+    const reader = new FileReader()
+
+    reader.onload = event => {
+      this.preview.set(event.target?.result?.toString() ?? '')
+    }
+
+    reader.readAsDataURL(file)
+
+  }
 
 }
