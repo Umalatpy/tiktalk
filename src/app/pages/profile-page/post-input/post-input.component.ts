ee616c22bdead023b0d63941e3a079c54274f06c
Umalat <samsamow@gmail.com>
2025-02-06 19:37:10 +0300

    post 6.50

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AvatarCircleComponent} from '../../../common-ui/avatar-circle/avatar-circle.component';
+import {ProfileService} from '../../../data/services/profile.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-post-input',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    NgIf
+  ],
   templateUrl: './post-input.component.html',
   styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
-
+  profile = inject(ProfileService).me
 }
